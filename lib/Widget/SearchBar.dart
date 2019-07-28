b152106941ad752988adcf70ca0a2e4ee916486a
Jay <forever84721@yahoo.com.tw>
2019-07-28 19:01:52 +0800

    .

--- lib/Widget/SearchBar.dart
+++ lib/Widget/SearchBar.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatefulWidget implements PreferredSizeWidget {
-  final String foo;
-  const SearchBar({Key key, this.foo}) : super(key: key);
+  final String title;
+  const SearchBar({Key key, this.title}) : super(key: key);
   @override
   _SearchBarState createState() => _SearchBarState(); //foo: this.foo
 
@@ -23,7 +23,7 @@ class _SearchBarState extends State<SearchBar> {
   @override
   void initState() {
     setState(() {
-      appBarTitle = new Text(widget.foo);
+      appBarTitle = new Text(widget.title);
     });
     super.initState();
   }
@@ -53,7 +53,7 @@ class _SearchBarState extends State<SearchBar> {
                   );
                 } else {
                   this.actionIcon = new Icon(Icons.search);
-                  this.appBarTitle = new Text(widget.foo);
+                  this.appBarTitle = new Text(widget.title);
                 }
               });
             },
